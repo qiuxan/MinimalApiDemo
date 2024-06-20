a0ea9866078334ab8073bd950e590c87a1f6ecd7
qiuxan <qiuxan@gmail.com>
2024-06-20 16:34:43 +1000

    create CRUD example in program.cs file

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -36,9 +36,78 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+var list = new List<Post>()
+{
+    new() { Id = 1, Title = "First Post", Content = "Hello World" },
+    new() { Id = 2, Title = "Second Post", Content = "Hello Again" },
+    new() { Id = 3, Title = "Third Post", Content = "Goodbye World" },
+};
+
+
+app.MapGet("/posts",() => list).WithName("GetPosts").WithOpenApi().WithTags("Posts");
+
+app.MapPost(
+    "/posts",
+    (Post post) =>
+        {
+            list.Add(post);
+            return Results.Created($"/posts/{post.Id}", post);
+        }
+    ).WithName("CreatePost").WithOpenApi().WithTags("Posts");
+
+
+app.MapGet(
+    "/posts/{id}",
+    (int id) =>
+        {
+            var post = list.FirstOrDefault(p => p.Id == id);
+            return post == null ? Results.NotFound() : Results.Ok(post);
+        })
+    .WithName("GetPost").WithOpenApi().WithTags("Posts");
+
+
+app.MapPut(
+    "/posts/{id}", 
+    (int id, Post post) =>
+        {
+            var index = list.FindIndex(p => p.Id == id);
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+            list[index] = post;
+            return Results.Ok(post);
+        })
+    .WithName("UpdatePost").WithOpenApi().WithTags("Posts");
+
+
+app.MapDelete(
+    "/posts/{id}", 
+    (int id) =>
+        {
+            var post = list.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return Results.NotFound();
+            }
+            list.Remove(post);
+            return Results.Ok();
+        }).WithName("DeletePost").WithOpenApi().WithTags("Posts");
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
+
+
+
+
